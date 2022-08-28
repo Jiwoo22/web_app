8f7153f1214081e087b85a15b04229f691331042
Jiwoo Choi <jchoi136@gmail.com>
2022-08-28 15:33:40 -0700

    add for-sale-items components

---
+++ components/for-sale-items/ForSaleItem.js
@@ -0,0 +1,24 @@
+import Card from "../ui/Card";
+import classes from "./ForSaleItem.module.css";
+import Image from "next/image";
+
+const ForSaleItem = (props) => {
+  return (
+    <li className={classes.item}>
+      <Card>
+        <div className={classes.image}>
+          <img src={props.image} alt={props.description} />
+        </div>
+        <div className={classes.content}>
+            <h3>{props.title}</h3>
+            <h3>{props.price}</h3>
+        </div>
+        <div className={classes.action}>
+            <button>Send Message</button>
+        </div>
+      </Card>
+    </li>
+  );
+};
+
+export default ForSaleItem;
